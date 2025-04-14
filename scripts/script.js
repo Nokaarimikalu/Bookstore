7e269c4713a93ad62e82c1c2ea8b2591c7c6df83
Pascal <fliednerpascal@gmail.com>
2025-04-14 20:08:38 +0200

    v2

---
+++ scripts/script.js
@@ -0,0 +1,208 @@
+// #region Arrays
+
+let books = [
+    {
+        name: "Die Geheimnisse des Ozeans",
+        author: "Clara Meer",
+        likes: 1250,
+        liked: true,
+        price: 19.99,
+        publishedYear: 2018,
+        genre: "Fantasy",
+        image: "/assets/img/81DliYtUJqL._SY342_.jpg",
+        comments: [
+            {
+                name: "Leser123",
+                comment: "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+            },
+            {
+                name: "Bookworm84",
+                comment: "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+            },
+            {
+                name: "FantasyFanatic",
+                comment: "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+            },
+            {
+                name: "SciFiGuru",
+                comment: "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+            },
+            {
+                name: "NovelLover",
+                comment: "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+            },
+        ],
+    },
+    {
+        name: "Der vergessene Pfad",
+        author: "Maxmilian Schwarz",
+        likes: 980,
+        liked: false,
+        price: "14.50",
+        publishedYear: 2021,
+        genre: "Fantasy",
+        image: "/assets/img/719sInWrmML._SY522_.jpg",
+        comments: [],
+    },
+    {
+        name: "Die Farben des Himmels",
+        author: "Laura Blau",
+        likes: 1520,
+        liked: true,
+        price: 22.95,
+        publishedYear: 2019,
+        genre: "Romantik",
+        image: "/assets/img/41c0eGqunkL._SY445_SX342_.jpg",
+        comments: [
+            {
+                name: "LeserPeter",
+                comment: "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt.",
+            },
+            {
+                name: "BookLover21",
+                comment: "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat.",
+            },
+            {
+                name: "FantasyNerd",
+                comment: "Fantastische Welten und epische Abenteuer - genau mein Geschmack!",
+            },
+            {
+                name: "SciFiEnthusiast",
+                comment: "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht.",
+            },
+            {
+                name: "ReadingAddict",
+                comment: "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat.",
+            },
+        ],
+    },
+    {
+        name: "Das Rätsel der Zeit",
+        author: "Alexander Weiss",
+        likes: 750,
+        liked: false,
+        price: 18.0,
+        publishedYear: 2020,
+        genre: "Science-Fiction",
+        image: "/assets/img/141rLQJm8g7L._SY445_SX342_.jpg",
+        comments: [
+            {
+                name: "BuchKenner",
+                comment: "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat.",
+            },
+            {
+                name: "LeseWurm",
+                comment: "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben.",
+            },
+        ],
+    },
+    {
+        name: "Der letzte Wächter",
+        author: "Sabine Grün",
+        likes: 1300,
+        liked: true,
+        price: 16.75,
+        publishedYear: 2017,
+        genre: "Fantasy",
+        image: "/assets/img/der letzte.jpg",
+        comments: [],
+    },
+    {
+        name: "Im Schatten des Mondes",
+        author: "Philipp Silber",
+        likes: 890,
+        liked: false,
+        price: 12.3,
+        publishedYear: 2022,
+        genre: "Science-Fiction",
+        image: "/assets/img/images.jpeg",
+        comments: [
+            {
+                name: "BücherLiebhaber",
+                comment: "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd.",
+            },
+            {
+                name: "Leseratte",
+                comment: "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat.",
+            },
+        ],
+    },
+    {
+        name: "Jenseits der Sterne",
+        author: "Oliver Schwarz",
+        likes: 1450,
+        liked: true,
+        price: 21.0,
+        publishedYear: 2015,
+        genre: "Science-Fiction",
+        image: "/assets/img/Download.jpeg",
+        comments: [
+            {
+                name: "Leser123",
+                comment: "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat.",
+            },
+        ],
+    },
+    {
+        name: "Das verborgene Königreich",
+        author: "Elena Gold",
+        likes: 920,
+        liked: false,
+        price: 17.5,
+        publishedYear: 2020,
+        genre: "Fantasy",
+        image: "/assets/img/Download (1).jpeg",
+        comments: [
+            {
+                name: "Bookworm92",
+                comment: "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat.",
+            },
+        ],
+    },
+    {
+        name: "Liebe in Zeiten des Krieges",
+        author: "Emilia Rot",
+        likes: 1800,
+        liked: true,
+        price: 19.99,
+        publishedYear: 2016,
+        genre: "Romantik",
+        image: "/assets/img/Download (2).jpeg",
+        comments: [
+            {
+                name: "Bibliophile23",
+                comment: "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen.",
+            },
+            {
+                name: "StorySeeker",
+                comment: "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat.",
+            },
+            {
+                name: "SciFiExplorer",
+                comment: "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig.",
+            },
+        ],
+    },
+];
+// #endregion
+let globaleVariable = 0 - 1;
+
+function init() {
+    renderBookTemplate();
+}
+
+
+function renderBookTemplate() {
+    const bookTemp = document.getElementById(`hauptbereich`);
+    let currentIndex = books.findIndex(book => book.name === "Die Geheimnisse des Ozeans")
+
+    for (let bookAmount = 0; bookAmount < books.length; bookAmount++) {
+        bookTemp.innerHTML += bookTemplate(currentIndex);//hier koennen mehr funktionen stehen
+        currentIndex++
+    }
+    
+    
+    
+}
+
+
---
+++ scripts/template.js
@@ -0,0 +1,51 @@
+function bookTemplate(i) {
+    return `<div class="templateBook" id="${[i+1]}">
+                <div class="nameOfTheBook"><h2>${books[i].name}</h2></div>
+                <div class="imageOfTheBook">
+                    <img src="${books[i].image}" alt="" />
+                </div>
+                <div class="infoOfTheBook">
+                    <div class="priceAndLikes">
+                        <div class="price"><h2>${books[i].price}$</h2></div>
+                        <div class="likes"><span id="likedNumber">${books[i].likes}</span> <img onclick="heartClick()" src="./assets/img/black-heart.png" alt="" /></div>
+                    </div>
+                    <table class="infoBook">
+                        <tr>
+                            <td><b>Autor</b></td>
+                            <td>: ${books[i].author}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Erscheinungsjahr</b></td>
+                            <td>: ${books[i].publishedYear}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Genre</b></td>
+                            <td>: ${books[i].genre}</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="whiteLine"></div>
+                <div class="commentsOfTheBook">
+                    <span> <b>Kommentare:</b></span>
+                    <table>
+                        <tr>
+                            <td>Kevin</td>
+                            <td>: asadfd</td>
+                        </tr>
+                        <tr>
+                            <td>Kevin</td>
+                            <td>: asadfd</td>
+                        </tr>
+                        <tr>
+                            <td>Kevin</td>
+                            <td>:asadsssssssssssssss</td>
+                    </table>
+                </div>
+                <div class="inputOfTheBook">
+                    <form><input class="comments" type="text" placeholder="Schreibe dein Kommentar..." /></form>
+                    <a href="#">
+                        <img src="./assets/img/telegram-logo.png" alt="" />
+                    </a>
+                </div>
+            </div>`;
+}
